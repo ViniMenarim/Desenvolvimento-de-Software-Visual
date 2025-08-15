1fed85f70205a2ade199a148869d8d3b3c5115aa
ViniMenarim <vinicius.junior005@cs.up.edu.br>
2025-08-15 10:57:24 -0300

    Atividade de Ordenação de Vetor
    Atividade de 15/08/2025

---
+++ Atividade de Ordenação de Vetor/Solution/Project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int[] array = new int[1000];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 1001);
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine("Valores antes da ordenação: ");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Array.Sort(array);
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+        Console.WriteLine("Valores depois da ordenação: ");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
+
